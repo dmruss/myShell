64760b7452b30e57e37b942c1d8a16b078bc7861
David Russell <45084308+dmruss@users.noreply.github.com>
2020-10-05 17:41:48 -0600

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+void initialize() {
+  cout << "my_shell>>";
+}
+
+int main() {
+
+  string input;
+
+  initialize();
+  while(true) {
+
+    
+
+  }
+
+  cout << "Hello World";
+  return 0;
+}
